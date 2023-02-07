0db1f3a2eea75a804184ca41e178e7726016d84c
Ryan P <rdpinkstaff@gmail.com>
2023-02-06 19:12:46 -0700

    Need to get admin delete working

--- models/user.js
+++ models/user.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     membership: { type: Boolean, required: true},
+    admin: { type: Boolean },
 })
 
 module.exports = mongoose.model("User", UserSchema);
